59394971b40b64f62951a9ef8acb937e9fa07331
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-08-06 12:09:39 -0300

    primeiro commit1

---
+++ src/main/java/com/eleodoro/dispenca_eleodoro/DispencaEleodoroApplication.java
@@ -0,0 +1,15 @@
+package com.eleodoro.dispenca_eleodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
+public class DispencaEleodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispencaEleodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/dispenca_eleodoro/DispencaEleodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.dispenca_eleodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispencaEleodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
