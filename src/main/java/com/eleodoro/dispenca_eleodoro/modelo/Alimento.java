edc9155b9a48d6646bdfabc05544f169d38f335e
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-08-19 12:18:52 -0300

    Criada classe Alimento

---
+++ src/main/java/com/eleodoro/dispenca_eleodoro/modelo/Alimento.java
@@ -0,0 +1,75 @@
+package com.eleodoro.dispenca_eleodoro.modelo;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Alimento implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+
+    public Alimento() {
+    }
+
+    public Alimento(String nome) {
+        this.nome = nome;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Alimento [id=" + id + ", nome=" + nome + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Alimento other = (Alimento) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+    
+}
