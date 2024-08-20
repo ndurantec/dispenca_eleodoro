b2b0962ad69fa6c5e6868b783a2a172784067235
pimentelcf <vitor.pimentel.santos@escola.pr.gov.br>
2024-08-20 12:05:18 -0300

    controllerLote add

--- src/main/java/com/eleodoro/dispenca_eleodoro/controller/LoteController.java
+++ src/main/java/com/eleodoro/dispenca_eleodoro/controller/LoteController.java
@@ -1,5 +1,28 @@
 package com.eleodoro.dispenca_eleodoro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Lote")
+
+
+
 public class LoteController {
      
+    @GetMapping(value = "/imprimir")
+    public void imprimir(){
+
+        System.out.println("chegou ate aqui o LoteController");
+
+
+
+
+    }
+
+
+
+
+
 }
