654b1def9ebe898ec88de3ccea50deb88182b755
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-08-20 11:54:56 -0300

    ajustando Controller Pedido e DetalhePedido

--- src/main/java/com/eleodoro/dispenca_eleodoro/controller/DetalhePedidoController.java
+++ src/main/java/com/eleodoro/dispenca_eleodoro/controller/DetalhePedidoController.java
@@ -1,5 +1,16 @@
 package com.eleodoro.dispenca_eleodoro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/DetalhePedido")
 public class DetalhePedidoController {
+
+    @GetMapping(value = "impremir")
+    public void impremir(){
+        System.out.println("Deu certo o controller DetalhePedido");
+    }
     
 }
--- src/main/java/com/eleodoro/dispenca_eleodoro/controller/PedidoController.java
+++ src/main/java/com/eleodoro/dispenca_eleodoro/controller/PedidoController.java
@@ -1,5 +1,15 @@
 package com.eleodoro.dispenca_eleodoro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Pedido")
 public class PedidoController {
     
+    @GetMapping(value = "impremir")
+    public void impremir(){
+        System.out.println("Deu certo o controller Pedido");
+    }
 }
