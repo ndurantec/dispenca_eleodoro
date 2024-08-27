35afa32dc09e331e8771f3e9309be2d849c05310
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-08-27 11:29:16 -0300

    letras arrumadas

--- src/main/java/com/eleodoro/dispenca_eleodoro/controller/DetalhePedidoController.java
+++ src/main/java/com/eleodoro/dispenca_eleodoro/controller/DetalhePedidoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/DetalhePedido")
+@RequestMapping(value = "/detalhepedido")
 public class DetalhePedidoController {
 
     @GetMapping(value = "/imprimir")
