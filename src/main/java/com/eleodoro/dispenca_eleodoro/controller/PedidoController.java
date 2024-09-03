2590f9b0e3b77d55c883dbff712bd5ba38c79a97
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-09-03 11:54:34 -0300

    testando insert

--- src/main/java/com/eleodoro/dispenca_eleodoro/controller/PedidoController.java
+++ src/main/java/com/eleodoro/dispenca_eleodoro/controller/PedidoController.java
@@ -1,8 +1,12 @@
 package com.eleodoro.dispenca_eleodoro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eleodoro.dispenca_eleodoro.dto.PedidoDTO;
 
 @RestController
 @RequestMapping(value = "/pedido")
@@ -11,5 +15,13 @@ public class PedidoController {
     @GetMapping(value = "/imprimir")
     public String imprimir(){
         return "chegou ate aqui Pedido";
+    }
+
+
+    @PostMapping(value = "/insert")
+    public void insert(@RequestBody PedidoDTO pedidoDTO){
+
+        System.out.println("chegou no Metodo insert");
+        System.out.println(pedidoDTO.toString());
     }
 }
